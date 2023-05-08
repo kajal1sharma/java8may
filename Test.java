80413f6e0c8756bd6a7da993535c9fbefe612bf9
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-08 20:52:37 +0530

    ..

--- Test.java
+++ Test.java
@@ -1,5 +1,39 @@
 class Test{
     public static void main(String[] args) {
-        System.out.println("hello this is my first program");
-    }
+
+
+        int a = 10; //data type  identifier  =(assignment operator)  literal(constant
+        int b = a;// datatype variavle =(assign op) variable
+       
+
+        byte bt= 12;
+        //8bits=> 1byte =>  0(1st bit => sign ) 0 0 0 0 0 0 0 
+
+
+        //byte short int long float double boolean char
+        //byte short int long => integer values
+        //float double =>(floating point values 56.344)
+        //boolean =>(true/false)
+        //char => 'a','b'
+
+
+
+        //declation 
+
+
+        // int a=10;
+        // a=89;
+        // a=56;
+        // a=34;
+
+
+        //there are 4 identifiers => Test(class), main(function), String(class) , args(variable) 
+
+    //    System.out.println("hello wrold");
+    //    System.out.println("helllo  this is my second line of code");
+
+    //identifier => something which is used to identify 
+//     =>class, fucntion , variables, inteface
+// ,enums    
+}
 }

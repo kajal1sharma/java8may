7f4f2837ba490cb7c54e78f8a7662951624abf6e
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-10 20:55:28 +0530

    operators

---
+++ Operators.java
@@ -0,0 +1,35 @@
+public class Operators {
+    public static void main(String[] args) {
+         //bbolean expression
+         //what is a block 
+         //>,< 
+        int a=90;
+
+         if(a<=90){
+          System.out.println("print if statement");  
+        }
+        else if(a>=90){
+            int b=90;
+            b=b+90;
+            System.out.println(b);
+            System.out.println("print else if statement");
+        }
+        else if(a!=90){
+            int b=90;
+            b=b+90;
+            System.out.println(b);
+            System.out.println("print else if statement");
+        }
+        else if(a==90){
+            int b=90;
+            b=b+90;
+            System.out.println(b);
+            System.out.println("print else if statement");
+        }
+        else{
+            System.out.println("print else statement");
+        }
+
+
+    }
+}
--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 class Test{
     public static void main(String[] args) {
 
+        
 
 
         boolean b=true;
